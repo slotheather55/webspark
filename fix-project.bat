07933064642738f08ca6a9dcc78f28d35b102714
slotheather55 <rafi@yismach.com>
2025-02-27 23:08:03 -0500

    Clean up unnecessary files in project root

--- fix-project.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Cleaning up project...
-rmdir /s /q node_modules
-del package-lock.json
-
-echo Installing dependencies...
-npm install
-
-echo Project has been fixed. Run npm start to start your application. 
